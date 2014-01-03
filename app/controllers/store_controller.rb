fdf55b0a0f3dd3adb5971b0b2633d97270d98211
jnears <jnears@essex.ac.uk>
2014-01-03 19:34:27 +0000

    Chapter 9

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
+	include SessionCounter #inlcude the concern session_counter.rb 
+	
   def index
   	@products = Product.order(:title)
+  	@counter = increment_session_counter #increase the counter by 1 and update the counter variable
   end
+
 end
